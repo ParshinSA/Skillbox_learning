83b8aac31015609c7b65466e1326ccd27fcc18f2
ParshinSA <Parshin.Sergey222@mail.ru>
2022-06-16 12:15:22 +0300

    1) Introduction to Kotlin, practical work

---
+++ 1) Introduction to Kotlin, practical work/app/src/main/java/com/example/introductiontokotlinpracticalwork/Main.kt
@@ -0,0 +1,16 @@
+package com.example.introductiontokotlinpracticalwork
+
+fun main() {
+//    print("What is your name?")
+
+    /*Вопрос*/
+    println("What is your name?")
+
+    // Ответ
+    print("My name is ")
+    println("Parshin Sergey")
+
+    /**
+     * Мой уровень программирования "Ученик"
+     * */
+}
